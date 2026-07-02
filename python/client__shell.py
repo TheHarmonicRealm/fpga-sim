d000809c7f64965a9853183b2fed9736a12c7148
Noah <44441682+TheHarmonicRealm@users.noreply.github.com>
2026-07-02 16:59:59 -0700

    Add warning if user tries to compile code with a $write

--- python/client__shell.py
+++ python/client__shell.py
@@ -191,6 +191,13 @@ def build_live_sim(input_files: list[NamedFile], folder_name: str, mode: str):
 
     if mode not in simulators_map.keys():
         raise ContinueException(f"There is no simulator named {mode}")
+    
+    for file in input_files:
+        if "$write" in file.content:
+            if not prompt_y_N(f"input file {file.name} appears to contain a $write "
+                "call; if your program prints output without a newline at the "
+                "end, it is very likely to crash this app.", "Build"):
+                return False
 
     print("Generating header...", end="", flush=True)
 
