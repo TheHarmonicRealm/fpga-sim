2034d811bb3a34bb28c98d55bf39aae025d857e6
Noah <44441682+TheHarmonicRealm@users.noreply.github.com>
2026-07-03 08:55:48 -0700

    Remove verilog templates folder

--- templates/one_digit.v
+++
@@ -1,8 +0,0 @@
-`timescale 100ms / 10ms
-module top(
-    input [15:0]         switches,
-    output reg [6:0]         segment,
-    output reg               DP,
-    output reg [15:0]        lights);
-
-endmodule
