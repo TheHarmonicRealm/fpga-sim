e0470ff3d201f000c2f914f5e8727ffde4465592
Noah <44441682+TheHarmonicRealm@users.noreply.github.com>
2026-08-31 21:14:00 -0700

    Move docker tag to top folder

--- python/client__paths.py
+++ python/client__paths.py
@@ -8,9 +8,9 @@ verilog_folder = top_folder.joinpath("verilog")  # fpga-sim/verilog/
 live_sim_folder = verilog_folder.joinpath("live_sim")
 testbench_folder = verilog_folder.joinpath("testbench")
 
-docker_tag_filepath = python_folder.joinpath("docker_tag.txt")
 board_data = python_folder.joinpath("board_data.toml")
 user_board_data = python_folder.joinpath("board_data_user.toml")
 apple_game_svg = python_folder.joinpath("svg/apple_game.svg")
 
 settings_toml = top_folder.joinpath("settings.toml")
+docker_tag_filepath = top_folder.joinpath("docker_tag.txt")
